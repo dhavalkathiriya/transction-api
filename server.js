f45398715dcf48e3d7e5a91972e8935e99c27b64
dhaval kathiriya <112376785+dhavalkathiriya@users.noreply.github.com>
2023-06-25 10:07:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import connectDb from "./db/db";
 import authRoute from "./routes/AuthRoute";
 import TransctionRoute from "./routes/TransctionRoute";
 import { PORT } from "./config";
-import path from 'path'
 
 connectDb();
 
@@ -15,13 +14,6 @@ app.use(express.json());
 
 app.use("/api/auth",authRoute);
 app.use("/api/transction", TransctionRoute);
-
-// static files
-app.use(express.static(path.join(__dirname, "./client/dist")));
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
